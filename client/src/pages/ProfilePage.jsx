bfb7442b00f7c9831eba8237a65d7fb2bb1122ad
Karen Schick <karen.schick6@gmail.com>
2024-03-11 16:53:09 -0400

    profile page styling updated

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -53,43 +53,70 @@ export default function ProfilePage(props) {
   return (
     <>
       <Container className="mt-3 " style={{ width: "30%" }}>
-                   
-            {user && (
-              <Card bg="header" className="text-center">
-                <Card.Body>
-                  <Row><Col>
-                  <Figure className="bg-border-color overflow-hidden my-auto ml-2 p-1" style={{ height: "100px", width: "100px" }}>
-                    <Figure.Image src={user.profile_image} style={{ borderRadius: "0%", height: "100%", width: "auto", objectFit: "cover" }} />
-                  </Figure></Col>
-                  <Col>
+        {user && (
+          <Card bg="header" className="text-center">
+            <Card.Body>
+              <Row>
+                <Col>
+                  <Figure
+                    className="bg-border-color overflow-hidden my-auto ml-2 p-1"
+                    style={{ height: "100px", width: "100px" }}
+                  >
+                    <Figure.Image
+                      src={user.profile_image}
+                      style={{
+                        borderRadius: "0%",
+                        height: "100%",
+                        width: "auto",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </Figure>
+                </Col>
+                <Col>
                   <Card.Text className="mb-3">{user.username}</Card.Text>
                   <Card.Text className="mb-3">{user.email}</Card.Text>
                   <Card.Text className="mb-3">{user.zipcode}</Card.Text>
-                  </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-            )}
-          
-            {user && (
-              <Card className="mt-3" style={{ width: "50%" }}>
-                <Card.Body>
-                  <Figure className="bg-border-color overflow-hidden my-auto  p-1" style={{ height: "100px", width: "100px" }}>
-                    <Figure.Image src={user.dog.images && user.dog.images[0]} style={{ borderRadius: "0%", height: "100%", width: "auto", objectFit: "cover" }} />
-                  </Figure>
-                  <Card.Text className="mb-3">{user.dog.name}</Card.Text>
-                  <Card.Text className="mb-3">{user.dog.breed}</Card.Text>
-                  <Card.Text className="mb-3">{user.dog.size}</Card.Text>
-                </Card.Body>
-              </Card>
-            )}
-          
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        )}
+
+        {user && (
+          <Card className="mt-3" style={{ width: "50%" }}>
+            <Card.Body>
+              <Figure
+                className="bg-border-color overflow-hidden my-auto  p-1"
+                style={{ height: "100px", width: "100px" }}
+              >
+                <Figure.Image
+                  src={user.dog.images && user.dog.images[0]}
+                  style={{
+                    borderRadius: "0%",
+                    height: "100%",
+                    width: "auto",
+                    objectFit: "cover",
+                  }}
+                />
+              </Figure>
+              <Card.Text className="mb-3">{user.dog.name}</Card.Text>
+              <Card.Text className="mb-3">{user.dog.breed}</Card.Text>
+              <Card.Text className="mb-3">{user.dog.size}</Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+
         {state.user && state.user.username === params.uname && (
           <div className="mt-3">
             <Button
               variant="primary"
               className="d-inline-block"
-              style={{ border: "none", color: "white", display: "inline-block" }}
+              style={{
+                border: "none",
+                color: "white",
+                display: "inline-block",
+              }}
               onClick={() => navigate(`/profile/u/${params.uname}/edit`)}
             >
               Edit Profile
